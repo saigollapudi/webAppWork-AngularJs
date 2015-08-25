38f25890b399125725b29309e138da53f01c4956
saigollapudi <saigollapudi1@gmail.com>
2015-08-25 12:56:48 +0530

    final modification... code moved to webAppWork repository. Make SweetSai.org file to see these files

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-var app = angular.module('Todo', []);
+var app = angular.module('GetPage', ['ionic', 'app.controllers', $httpProvider]);
 
-app.controller('TodoCtrl', function ($scope) { 
-   $scope.newTodo ='';	
-   $scope.message = 'Sairam on Angular!';
-   $scope.todos = ['todo1', 'todo item2', 'item3'];
-   
-   $scope.done = function(todo) {
-	   var indexOf = $scope.todos.indexOf(todo);
-	   if (indexOf !== -1) {
-		   $scope.todos.splice(indexOf, 1);
-	      }
-	   };
-   $scope.add =function(e) { 
-	   if (e.which && e.which === 13) {
-		   $scope.todos.push($scope.newTodo);
-		   $scope.newTodo ='';
-		   }
-	   };	   
-   });
+//setup for allowing for CORS  
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.headers.common = 'Content-Type: application/json';
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }]);
+    
+//exposing expose variables and functionality to expressions and directives in Template
+app.controller('GetPageController', function ($scope, $http) { 
+    var myHeaders = {'Access-Control-Allow-Origin' : '*',
+				   'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+				   'Content-Type': 'application/json',
+				   'Accept': 'application/json',
+				   'Access-Control-Max-Age': '86400',    // cache for 1 day
+			      };
 
+	$scope.inputURL = "http://teststore.swtr.us/";
+	$scope.show = function() {
+		// var returnPromise = $http.get("http://teststore.swtr.us/");
+		var returnPromise = $http.get(headers:myHeaders, url:$scope.inputURL);
+		returnPromise.then(
+		    //success
+		    function(data, status, headers, config) {
+			   console.log("Server responded: Success in getting: ", $scope.inputURL);	
+			   $scope.expression = data;
+			   },
+			//error   
+		    function(data, status, headers, config) {
+               log($scope.inputURL);	 	
+			   console.log("Server responded: Error in getting: ", $scope.inputURL);
+			   },
+			//progress   
+			function(data, status, headers, config) {
+			   console.log("Server responded: Progress in getting: ", $scope.inputURL);	
+			   });
+		log($scope.inputURL);	   
+		console.log("I created an Asynch call and am exiting the Show() function");	
+		};
+	});
+
+
+function log(str){
+  var log = document.getElementById("log")
+  if (log){ 
+	 // let's be safe...
+     log.innerHTML += str + "<br/>";
+     }
+};
